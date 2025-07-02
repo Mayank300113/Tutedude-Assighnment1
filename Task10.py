400f0606d32ccaa83092eb8863b76902851eabf5
Mayank300113 <mayanksexperiverse@gmail.com>
2025-07-02 20:52:51 +0530

    Add files via upload

---
+++ Task10.py
@@ -0,0 +1,10 @@
+# Get input and convert to float
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform and print calculations directly
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+input("press any key to exit")
